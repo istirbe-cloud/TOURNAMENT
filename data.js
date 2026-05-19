0882997005b3fb8e55fcd1b0967b3d18e23c7cdc
istirbe-cloud <istirbe@educacion.navarra.es>
2026-05-20 01:07:09 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,227 @@
+{
+  "teams": [
+    {
+      "name": "PALM STRIKE SQUAD",
+      "img": "https://placeholder.com",
+      "members": [
+        "PAULA BEORLEGUI",
+        "ALICE NAVARRO",
+        "FLORENCIO IBIRICU",
+        "MIGUEL ARASTI",
+        "DOUA NAKIR",
+        "ROUMAISSAE AZOUIAR"
+      ],
+      "w": 3,
+      "d": 1,
+      "l": 0,
+      "pts": 10,
+      "pj": 4
+    },
+    {
+      "name": "SWIFT STRIKERS",
+      "img": "https://placeholder.com",
+      "members": [
+        "XABIER MONZON",
+        "MARTINA MORA",
+        "PABLO AYERRA",
+        "MAISSAE BELOUALI",
+        "UXUE AZCONA"
+      ],
+      "w": 1,
+      "d": 0,
+      "l": 2,
+      "pts": 3,
+      "pj": 3
+    },
+    {
+      "name": "COURT TITANS",
+      "img": "https://placeholder.com",
+      "members": [
+        "JULIA PROTOMARTIR",
+        "MARTÍN DURÁN",
+        "AMAIA ESPINOSA",
+        "DANIEL AGUERRI",
+        "MARA REDONDO",
+        "BASMA KARMAOUI"
+      ],
+      "w": 0,
+      "d": 1,
+      "l": 3,
+      "pts": 1,
+      "pj": 4
+    },
+    {
+      "name": "POWER HITTERS",
+      "img": "https://placeholder.com",
+      "members": [
+        "NORA GARCIA",
+        "CHLOE GONZALEZ",
+        "CARLA PALACIOS",
+        "OIHANE MARTÍN",
+        "TOMÁS IRIBARREN",
+        "MARINA AGUERRI"
+      ],
+      "w": 2,
+      "d": 1,
+      "l": 1,
+      "pts": 7,
+      "pj": 4
+    },
+    {
+      "name": "FAIR PLAYERS",
+      "img": "https://placeholder.com",
+      "members": [
+        "IRAIA ARBIZU",
+        "VICTORIA HINOJOSA",
+        "SAIOA LORENZO",
+        "MARKEL AGUIRRE",
+        "IOSUA STIRBEI"
+      ],
+      "w": 1,
+      "d": 1,
+      "l": 1,
+      "pts": 4,
+      "pj": 3
+    }
+  ],
+  "matches": [
+    {
+      "tA": "FAIR PLAYERS",
+      "sA": 1,
+      "tB": "COURT TITANS",
+      "sB": 1,
+      "date": "2026-05-06",
+      "ref": ".",
+      "mvp": "SAIOA LORENZO",
+      "loc": "RED COURT 1",
+      "linA": "MARKEL AGUIRRE, SAIOA LORENZO, VICTORIA HINOJOSA, IRAIA ARBIZU, IOSUA STIRBEI",
+      "linB": "MARA REDONDO, AMAIA ESPINOSA, BASMA KARMAOUI, MARTÍN DURAN, JULIA PROTOMARTIR, DANIEL AGUERRI",
+      "goals": "SAIOA LORENZO, MARTIN DURAN"
+    },
+    {
+      "tA": "POWER HITTERS",
+      "sA": 0,
+      "tB": "PALM STRIKE SQUAD",
+      "sB": 0,
+      "date": "2026-04-29",
+      "ref": "SAIOA LORENZO & MARKEL AGUIRRE",
+      "mvp": "TOMAS IRIBARREN",
+      "loc": "RED COURT 2",
+      "linA": "NORA GARCIA, CHLOE GONZALEZ, CARLA PALACIOS, TOMAS IRIBARREN, OIHANE MARTIN, MARINA AGUERRI",
+      "linB": "FLORENCIO IBIRICU, ALICE NAVARRO, ROUMAISSAE AZOUIAR, MIGUEL ARASTI, DOUA NAKIR",
+      "goals": ""
+    },
+    {
+      "tA": "PALM STRIKE SQUAD",
+      "sA": 3,
+      "tB": "SWIFT STRIKERS",
+      "sB": 0,
+      "date": "2026-05-06",
+      "ref": " TOMAS IRIBARREN & NORA GARCIA",
+      "mvp": "FLORENCIO IBIRICU",
+      "loc": "RED COURT 2",
+      "linA": "PAULA BEORLEGUI, ALICE NAVARRO, FLORENCIO IBIRICU, MIGUEL ARASTI, DOUA NAKIR, ROUMAISSAE AZOUIAR",
+      "linB": "XABIER MONZON, MARTINA MORA, PABLO AYERRA, MAISSAE BELOUALI, UXUE AZCONA",
+      "goals": " FLORENCIO IBIRICU, FLORENCIO IBIRICU, MIGUEL ARASTI"
+    },
+    {
+      "tA": "COURT TITANS",
+      "sA": 3,
+      "tB": "PALM STRIKE SQUAD",
+      "sB": 9,
+      "date": "2026-05-06",
+      "ref": "IOSUA STIRBEI & MARKEL AGUIRRE",
+      "mvp": "MIGUEL ARASTI",
+      "loc": "RED COURT 1",
+      "linA": "JULIA PROTOMARTIR, MARTIN DURAN, AMAIA ESPINOSA, DANIEL AGUERRI, MARA REDONDO, BASMA KARMAOUI",
+      "linB": "FLORENCIO IBIRICU, MIGUEL ARASTI, ROUMAISSAE AZOUIAR, DOUA NAKIR, PAULA BEORLEGUI, ALICE NAVARRO",
+      "goals": "DANIEL AGUERRI, DANIEL AGUERRI,OWN GOAL, FLORENCIO IBIRICU, FLORENCIO IBIRICU, MIGUEL ARASTI, DANIEL AGUERRI, MIGUEL ARASTI, PAULA BEORLEGUI, MIGUEL ARASTI, PAULA BEORLEGUI, PAULA BEORLEGUI"
+    },
+    {
+      "tA": "FAIR PLAYERS",
+      "sA": 1,
+      "tB": "PALM STRIKE SQUAD",
+      "sB": 2,
+      "date": "2026-04-29",
+      "ref": "MAISSAE BELOUALI & PABLO AYERRA",
+      "mvp": "FLORENCIO IBIRICU",
+      "loc": "RED COURT 1",
+      "linA": "MARKEL AGUIRRE, SAIOA LORENZO, VICTORIA HINOJOSA, IOSUA STIRBEI, IRAIA ARBIZU",
+      "linB": "FLORENCIO IBIRICU, PAULA BEORLEGUI, ALICE NAVARRO, MIGUEL ARASTI, DOUA NAKIR, ROUMAISSAE AZOUIAR",
+      "goals": "FLORENCIO IBIRICU, SAIOA LORENZO, MIGUEL ARASTI"
+    },
+    {
+      "tA": "POWER HITTERS",
+      "sA": 2,
+      "tB": "SWIFT STRIKERS",
+      "sB": 1,
+      "date": "2026-05-06",
+      "ref": "VICTORIA HINOJOSA",
+      "mvp": "MARTINA MORA",
+      "loc": "RED COURT 2",
+      "linA": "NORA GARCIA, CHLOE GONZALEZ, CARLA PALACIOS, TOMAS IRIBARREN, OIHANE MARTIN, MARINA AGUERRI",
+      "linB": "XABIER MONZON, MARINA MORA, PABLO AYERRA, MAISSAE BELOUALI, UXUE AZCONA",
+      "goals": " CHLOE GONZALEZ, CARLA PALACIOS, MARTINA MORA"
+    },
+    {
+      "tA": "POWER HITTERS",
+      "sA": 0,
+      "tB": "FAIR PLAYERS",
+      "sB": 1,
+      "date": "2026-05-06",
+      "ref": ".",
+      "mvp": "SAIOA LORENZO",
+      "loc": "RED COURT 2",
+      "linA": "CARLA PALACIOS, CHLOE GONZALEZ, TOMAS IRIBARREN, OIHANE MARTIN, NORA GARCIA",
+      "linB": "SAIOA LORENZO, IOSUA STIRBEI, MARKEL AGUIRRE, VICTORIA HINOJOSA, IRAIA ARBIZU",
+      "goals": "SAIOA LORENZO"
+    },
+    {
+      "tA": "COURT TITANS",
+      "sA": 0,
+      "tB": "POWER HITTERS",
+      "sB": 2,
+      "date": "2026-04-29",
+      "ref": "MARTINA MORA & UCUE AZCONA",
+      "mvp": "OIHANE MARTIN",
+      "loc": "RED COURT 1",
+      "linA": "MARTIN DURAN, DANIEL AGUERRI, MARA REDONDO, AMAIA ESPINOSA, JULIA PROTOMARTIR, BASMA KARMAOUI",
+      "linB": "NORA GARCIA, CHLOE GONZALEZ, CARLA PALACIOS, OIHANE MARTIN, TOMAS IRIBARREN, MARINA AGUERRI",
+      "goals": "CHLOE GONZALEZ, OIHANE MARTIN"
+    },
+    {
+      "tA": "COURT TITANS",
+      "sA": 0,
+      "tB": "SWIFT STRIKERS",
+      "sB": 3,
+      "date": "2026-04-29",
+      "ref": "IOSUA STIRBEI, VICTORIA HINOJOSA & IRAIA ARBIZU",
+      "mvp": "UXUE AZCONA",
+      "loc": "RED COURT 2",
+      "linA": "MARTIN DURAN, DANIEL AGUERRI, MARA REDONDO, AMAIA ESPINOSA, JULIA PROTOMARTIR, BASMA KARMAOUI",
+      "linB": "PABLO AYERRA, MARTINA MORA, UXUE AZCONA, MAISSAE BELOUALI, XABIER MONZON",
+      "goals": "OWN GOAL, MARTINA MORA,  XABIER MONZON"
+    }
+  ],
+  "scheduledMatches": {
+    "May-7": [
+      "FAIR PLAYERS 1-1 COURT TITANS",
+      "PALM STRIKE SQUAD 3-0 SWIFT STRIKERS",
+      "COURT TITANS 3-9 PALM STRIKE SQUAD",
+      "POWER HITTERS 2-1 SWIFT STRIKERS",
+      "POWER HITTERS 0-1 FAIR PLAYERS"
+    ],
+    "April-30": [
+      "POWER HITTERS 0-0 PALM STRIKE SQUAD",
+      "FAIR PLAYERS 1-2 PALM STRIKE SQUAD",
+      "COURT TITANS 0-2 POWER HITTERS",
+      "COURT TITANS 0-3 SWIFT STRIKERS"
+    ]
+  },
+  "interviews": [],
+  "knockoutMatches": {
+    "semi1": null,
+    "semi2": null,
+    "final": null
+  }
+}
